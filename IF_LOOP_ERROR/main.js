d8a7395e2053a4b62bfbb7ccc9b01067bee9b522
susumumikan <susumu.mikan@outlook.jp>
2025-04-04 20:52:26 +0900

    論理演算のを追加

--- IF_LOOP_ERROR/main.js
+++ IF_LOOP_ERROR/main.js
@@ -14,4 +14,12 @@ maybeOK2 = 1 === 1;
 maybeOK2 = 1 !== 2;
 maybeOK2 = 1 == 0;
 maybeOK2 = 1 >= 2;
-console.log(maybeOK2); //trueが表示される
+maybeOK2 = true && false; //falseが表示される
+maybeOK2 = true || false; //論理積ｏｒ　trueが表示される
+console.log(maybeOK2); //論理和ａｎｄ　trueが表示される
+const userInput ="";
+const uerName = userInput || "User"; //論理和　falseが表示される
+console.log(uerName); //Userが表示される
+
+
+
