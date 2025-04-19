cf2354d2ecccee7aef3bb4aefe5e1ec74a38b203
susumumikan <susumu.mikan@outlook.jp>
2025-04-19 16:56:43 +0900

    javeの資料更新

--- IF_LOOP_ERROR/main.js
+++ IF_LOOP_ERROR/main.js
@@ -1,182 +1,183 @@
-let ok = false;
-let maybeOK = false;
-let maybeOK2 = true;
-if (ok) {
-    console.log("ok"); // "ok"は表示されない
-} else if (maybeOK) {
-    console.log("maybeOK..."); // "maybeOK..."は表示されない
-} else {
-    console.log("out"); // "out"が表示される
-}
+// let ok = false;
+// let maybeOK = false;
+// let maybeOK2 = true;
+// if (ok) {
+//     console.log("ok"); // "ok"は表示されない
+// } else if (maybeOK) {
+//     console.log("maybeOK..."); // "maybeOK..."は表示されない
+// } else {
+//     console.log("out"); // "out"が表示される
+// }
 
-maybeOK2 = 1 === 1; // true
-maybeOK2 = 1 !== 2; // true
-maybeOK2 = 1 == 0; // false
-maybeOK2 = 1 >= 2; // false
-maybeOK2 = true && false; // false
-maybeOK2 = true || false; // true
-console.log(maybeOK2); // true
+// maybeOK2 = 1 === 1; // true
+// maybeOK2 = 1 !== 2; // true
+// maybeOK2 = 1 == 0; // false
+// maybeOK2 = 1 >= 2; // false
+// maybeOK2 = true && false; // false
+// maybeOK2 = true || false; // true
+// console.log(maybeOK2); // true
 
-const userInput = "";
-const userName = userInput || "User"; // "User"
-console.log(userName); // "User"
+// const userInput = "";
+// const userName = userInput || "User"; // "User"
+// console.log(userName); // "User"
 
-const x = 15;
-ok = x > 10 && x < 20; // true
-ok = x === 10 || (x > 12 && userName); // true
-if (ok) {
-    console.log("OK"); // "OK"が表示される
-} else {
-    console.log("No"); // 表示されない
-}
+// const x = 15;
+// ok = x > 10 && x < 20; // true
+// ok = x === 10 || (x > 12 && userName); // true
+// if (ok) {
+//     console.log("OK"); // "OK"が表示される
+// } else {
+//     console.log("No"); // 表示されない
+// }
 
-let username = "" || "User"; // "User"
+// let username = "" || "User"; // "User"
 
-ok1 = !true; // false
-ok1 = !"hello"; // true
-ok1 = !0; // true
-ok1 = !!x; // true
-console.log(ok1); // false
+// ok1 = !true; // false
+// ok1 = !"hello"; // true
+// ok1 = !0; // true
+// ok1 = !!x; // true
+// console.log(ok1); // false
 
-const hello = "Hello!2";
-{
-    //console.log(hello); // "Hello!2"はデッドゾーンエラーになる
-    const hello = "Hello!1";
-    console.log(hello);
-}
+// const hello = "Hello!2";
+// {
+//     //console.log(hello); // "Hello!2"はデッドゾーンエラーになる
+//     const hello = "Hello!1";
+//     console.log(hello);
+// }
 
-//if (ton) ton = true;//()の中は定義　｛｝の中はどんな分でもOK
+// //if (ton) ton = true;//()の中は定義　｛｝の中はどんな分でもOK
 
-ok3 = true; //  ok3はtrue
-ok3 = ok ? "ok" : "no"; // 三項演算子という条件演算子を使った書き方
-console.log(ok3); // "ok"が表示される
+// ok3 = true; //  ok3はtrue
+// ok3 = ok ? "ok" : "no"; // 三項演算子という条件演算子を使った書き方
+// console.log(ok3); // "ok"が表示される
 
-function vegetbleColor(vegetable){
-    switch (vegetable){
-        case "tomato":
-            console.log("tomato is red.");
-            break;
-        case "pumpkin":
-            console.log("pumpkin is oernge.");
-            break;
-        case "onion":
-            console.log("onion is white.");
-            break;
-        default:
-            console.log("unknown vegetable.");
-    }
-    // if (vegetable === "tomato") {
-    // console.log("tomato is red.");
-    // }else if (vegetable === "pumpkin") {
-    //     console.log("pumpkin is oernge.");
-    // }else if (vegetable === "onion") {
-    //     console.log("onion is white.");
-    // }
-}
-vegetbleColor("tomato"); // "tomato is red."が表示される
+// function vegetbleColor(vegetable){
+//     switch (vegetable){
+//         case "tomato":
+//             console.log("tomato is red.");
+//             break;
+//         case "pumpkin":
+//             console.log("pumpkin is oernge.");
+//             break;
+//         case "onion":
+//             console.log("onion is white.");
+//             break;
+//         default:
+//             console.log("unknown vegetable.");
+//     }
+//     // if (vegetable === "tomato") {
+//     // console.log("tomato is red.");
+//     // }else if (vegetable === "pumpkin") {
+//     //     console.log("pumpkin is oernge.");
+//     // }else if (vegetable === "onion") {
+//     //     console.log("onion is white.");
+//     // }
+// }
+// vegetbleColor("tomato"); // "tomato is red."が表示される
 
-//while文
-let count = 100; //カウントの初期化
-while (count < 10) {
-    console.log("while: ", count); //カウントを表示
-    count += 1; //カウントを増やす
-}
+// //while文
+// let count = 100; //カウントの初期化
+// while (count < 10) {
+//     console.log("while: ", count); //カウントを表示
+//     count += 1; //カウントを増やす
+// }
 
-let tomato_count = 100; //カウントの初期化
-do {
-    console.log("do_while: ", tomato_count); //カウントを表示
-    tomato_count += 1; //カウントを増やす
-} while (tomato_count < 10); //do while文は必ず1回は実行される
+// let tomato_count = 100; //カウントの初期化
+// do {
+//     console.log("do_while: ", tomato_count); //カウントを表示
+//     tomato_count += 1; //カウントを増やす
+// } while (tomato_count < 10); //do while文は必ず1回は実行される
 
-//let pumpkin_count = 1; //for分にそのままいられる
-for (let pumpkin_count = 1;
-     pumpkin_count < 10;
-      pumpkin_count +=1) {
-    console.log("for: ", pumpkin_count,); //カウントを表示
-};//for文はカウントを初期化、条件を指定、カウントを増やす
+// //let pumpkin_count = 1; //for分にそのままいられる
+// for (let pumpkin_count = 1;
+//      pumpkin_count < 10;
+//       pumpkin_count +=1) {
+//     console.log("for: ", pumpkin_count,); //カウントを表示
+// };//for文はカウントを初期化、条件を指定、カウントを増やす
 
-const fruits = ["apple", "banana","grape", "mango", "orange"]; //配列の初期化
-for (let i = 0 ;i < fruits.length; i += 1) {
-    console.log(fruits[i]);
-}
-for (const fruit of fruits) {
-    console.log(fruit); //配列の要素を表示
-}
+// const fruits = ["apple", "banana","grape", "mango", "orange"]; //配列の初期化
+// for (let i = 0 ;i < fruits.length; i += 1) {
+//     console.log(fruits[i]);
+// }
+// for (const fruit of fruits) {
+//     console.log(fruit); //配列の要素を表示
+// }
 
-const banana ={
-    name: "banana Latte",
-    size: 350,
-    isHot: true,
-};
+// const banana ={
+//     name: "banana Latte",
+//     size: 350,
+//     isHot: true,
+// };
 
-for (const key in banana) {
-    console.log(key); //配列のキーを表示
-    console.log(banana[key]); //配列の値を表示
-    if (key === "size") {
-        console.log("break")
-        break; //break文でループを抜ける
-    }
-}
-const banana2 ={
-    name: "banana2 Latte",
-    size: 350,
-    isHot: true,
-};
+// for (const key in banana) {
+//     console.log(key); //配列のキーを表示
+//     console.log(banana[key]); //配列の値を表示
+//     if (key === "size") {
+//         console.log("break")
+//         break; //break文でループを抜ける
+//     }
+// }
+// const banana2 ={
+//     name: "banana2 Latte",
+//     size: 350,
+//     isHot: true,
+// };
 
-for (const key2 in banana2) {
-    if (key2 === "size") {
-        console.log("contiune")
-        continue; //continue文でループを抜ける
-    }
-    console.log(key2); //配列のキーを表示
-    console.log(banana2[key2]); //配列の値を表示
-}
+// for (const key2 in banana2) {
+//     if (key2 === "size") {
+//         console.log("contiune")
+//         continue; //continue文でループを抜ける
+//     }
+//     console.log(key2); //配列のキーを表示
+//     console.log(banana2[key2]); //配列の値を表示
+// }
 
-try{
-    console.log(chocolate); // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
-} catch {
-    console.log("hello"); // helloは表示される
-}
-
-
-
-try {} catch {} finally {}// finallyは必ず実行される
-
-try{
-    console.log("try"); // helloは表示される
-    console.log(chocolate); // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
-} catch {
-    console.log("catch"); // helloは表示される
-}
-finally {
-    console.log("finally"); // finallyは必ず実行される
-}
+// try{
+//     console.log(chocolate); // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
+// } catch {
+//     console.log("hello"); // helloは表示される
+// }
 
 
-function logChocolate() {
-    try{
-        console.log("try"); // helloは表示される
-        return "hello"; // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
-    } catch {
-        console.log("catch"); // helloは表示される
-    }
-    finally {
-        console.log("必ずfinally表示される"); // finallyは必ず実行される
-        return "returnも上書きfinally"; // finallyは必ず実行される
-    }
-}
-console.log(logChocolate()); // "try"が表示される
 
-//throw new Error("エラーが発生しました"); // エラーを投げる
+// try {} catch {} finally {}// finallyは必ず実行される
 
-function logChocolate2() {
-    try{
-        console.log("try"); // tryは表示されない
-        throw {message: "throw an error"}; // エラーを投げる
+// try{
+//     console.log("try"); // helloは表示される
+//     console.log(chocolate); // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
+// } catch {
+//     console.log("catch"); // helloは表示される
+// }
+// finally {
+//     console.log("finally"); // finallyは必ず実行される
+// }
 
-    } catch (error) {
-        console.log("catch", error); // "catch"が表示される
-    }
-}
-console.log(logChocolate2()); // "try"が表示される
+
+// function logChocolate() {
+//     try{
+//         console.log("try"); // helloは表示される
+//         return "hello"; // chocolateは定義されていないのでエラーになるがtry文でエラーをキャッチする
+//     } catch {
+//         console.log("catch"); // helloは表示される
+//     }
+//     finally {
+//         console.log("必ずfinally表示される"); // finallyは必ず実行される
+//         return "returnも上書きfinally"; // finallyは必ず実行される
+//     }
+// }
+// console.log(logChocolate()); // "try"が表示される
+
+// //throw new Error("エラーが発生しました"); // エラーを投げる
+
+// function logChocolate2() {
+//     try{
+//         console.log("try"); // tryは表示されない
+//         throw {message: "throw an error"}; // エラーを投げる
+
+//     } catch (error) {
+//         console.log("catch", error); // "catch"が表示される
+//     }
+// }
+// console.log(logChocolate2()); // "try"が表示される
+
 
--- OPEN_EDITORS/main.js
+++ OPEN_EDITORS/main.js
@@ -1,27 +1,72 @@
-// let count = 0;
-// console.log(count);
-// count = 30;
-// console.log(count);
-// let newCount = 0;
-// const daysInWeek = 7;
-// let tomatoCount;
+//レッスン１　JavaScriptの基礎
+//１．letを使って入れ物である変数を作る（変えられる物）
+//　〇「変数宣言」または「変数の初期化」という↓意味
+// let count = 0; //変数の前にはletを付ける必要がある。count1に０を代入するという
+// let count; //初期値に0を代入しない場合は、let count;と書くこともできる。初期値がundefined(未定義）になる。 
+// let countを２回宣言はできない。↑はそのまま使用するとエラーになるため、letは一度だけ宣言するようにする
+//変数の最後には「;」を付ける
+// console.log(count); //countの現在の値がどうなっているのか調べる方法としてconsole.logを使う＝０となっている
+// count = 30;  //countに30を代入するという意味 
+// console.log(count);  //countの現在の値がどうなっているのか調べる方法としてconsole.logを使う=３０となっている
+
+//２．constを使って、一定の値である定数を作る方法（変えられない物）＝基本は定数を使うようにする
+// let daysInWeek = 0; //変数宣言↓letをconstに変えると定数になる。
+// const daysInWeek = 7; //本来は、定数は一定の数になるものに使う
+// daysInWeek= 10; //定数は変更できないのでエラーになる。
+
+//３．変数や定数の名前のルールについて
+// let tomatoCount; //「―」「*」「%」「/」など使うことができないtomato-Countは使える
 // let $tomato_Count; //＄と_は使えるが、数字から始めることはできない
 // let _tomatoCount; //＄と_は使えるが、数字から始めることはできない
-// let _1tomatoCount; //数字から始めることはできない
+// let 1tomatoCount; //数字から始めることはできない
 // let console; //予約語は使えない
 // let トマトカウント; //日本語も使えるが、推奨されない
 // let tomatoCount1; //大文字と使うことをキャメルケースと言う
 // let tomato_count; //スネークケースも使えるが、キャメルケースが推奨される
 
-// let additionResult = 1 + 2;
-// let result = 0; //結果は0
-// result = result + 1; //結果は同じ
-// result += 1; //結果は同じ
-// console.log(result++); //結果は同じ
-// ++result; //結果は同じ
-// let string = "Hello" + "World";
-// console.log(string);
+//４．＋やーなどの算術演算子について
+// let additionResult = 1 + 2; //算術演算子という計算をするための記号を使うことができる。
+// +, - , * , / , %(余り), **(累乗) ,1+2*3（掛け算から先）などの記号を使うことができる。
 
+//５．　+=や++などの省略記号、演算子について
+// let result = 0; //変数の宣言（初期化）
+// result = result + 1; //resultはresultに１を足すという意味
+// result += 1; //+=は+=1と同じ意味のため、↑と同じ結果になる（-*/%なども同じ）
+// result++; //＋１の時だけ特別な書き方++は+1と同じ意味
+// ++result; //前に++時と、後ろに++時の違いは、後ろの方が値を返す前の値、前の方が値を返す後の値になる。
+// console.log(result); //resultの現在の値がどうなっているのか調べる方法としてconsole.logを使う=３となっている
+
+//６．「データの型」と「動的型付け」について
+// let number = 10; //数値型の変数の宣言
+// number = -4; //負の数も入る（intjeger型）
+// number = 0.4; //小数点も入る(float型）
+// let string = "Hello" + "World"; //文字列型の変数の宣言(string型）
+// console.log(string);
+// javascriptは動かすときに型を決める方法を動的型付け言語である
+// c言語はコンパイル時(コードの実行前）に型を決める方法を静的型付け言語である。
+
+
+//７．３つの文字列の書き方
+// const uerName = 'Susumu'; //シングルクォーテーションで囲む
+// const userName = "Susumu"; //ダブルクォーテーションで囲む
+// const userName = `Susumu`; //バッククォーテーションで囲む（テンプレートリテラル）
+// string = 'Hello' + userName + '!';  //文字列の結合
+// string = `Hello ${userName}!`; //テンプレートリテラルを使った文字列の結合
+// console.log(string); //文字列の結合を表示
+
+//８．どうやって型の変換が行われているか？
+// let number = 0;
+// console.log(number); //数値型の変数の宣言
+// number = "10"; //文字列型の変数の宣言
+// console.log(number); //文字列型の変数の宣言
+// number = 10 + "10"; //暗黙的にint型からstring型に変換される
+// console.log(number); //文字列型の変数の宣言（１０１０）
+// number = 10 - "10"; //暗黙的にstring型からint型に変換される
+// console.log(number); //数値型の変数の宣言（０）
+// number = "hello" * 10; //暗黙的にstring型からint型に変換される
+// console.log(number); //数値型の変数の宣言（NaN）
+
+//９．型の変換を明示的に行う方法
 // const userInput = "10.9"; //ユーザーからの入力を想定
 // let calcResult;
 // calcResult = Number(userInput) + 10; //Number関数を使って文字列を数値に変換
@@ -30,31 +75,43 @@
 // console.log(calcResult);
 // calcResult = parseFloat(userInput) + 10; //Number関数を使って文字列を数値に変換
 // console.log(calcResult);
+// calcResult = +userInput + 10; //Number関数を使って文字列を数値に変換
+// console.log(calcResult);
+// const tenNumber = 10; //数値型の変数の宣言
+// calcResult = "10" + String(tenNumber); //文字列型の変数の宣言
+// calcResult = "10" +tenNumber.toString(); //文字列型の変数の宣言
+// console.log(calcResult); //文字列型の変数の宣言（1010）
 
+//１０．真偽値の型について
 // let boolean = true; //真偽値
-// // boolean = false; //真偽値
-// // boolean = 1; //真偽値
-// // boolean = 0; //真偽値
+// boolean = false; //真偽値
+// boolean = 1; //真偽値
+// boolean = 0; //真偽値
 
-// let array = [1, 2, 3, 4, 5]; //配列の宣言
+
+//１１．配列の宣言と初期化
+// let array = ["apple", "banana", "grape"]; //配列の宣言と初期化
+// array = [1, 2, 3, 4, 5]; //配列の宣言
+// array = [1, "apple", true, null]; //中にはどんな型でも入る
 // array[0] = 10; //配列の値を変更
-// array = ["apple", "banana", "orange"]; //配列の値を変更
-// console.log(array[0]); //配列の値を表示
+// array = ["apple", "banana", "grape"]; //配列の値を変更
+// console.log(array[0]); //配列の値を表示一番最初の数値が１ではなく０になるため、appleが表示される
 // array = [];
 // array.push("apple");
 // array.push("banana");
 // array.push("grape");
 // console.log(array);
 
-// //オブジェクトの宣言
-// const coffee = {
-//   name: "Chocolate Mocha",
-//   price: 100,
-//   size: "M",
-//   isHot: true,
-//   toppings: ["chocolate", "whipped cream"],
-//   order: function () {
-//     console.log("Order: " + this.name + " " + this.size);
+//１２．もっとも重要な型、オブジェクトの使い、まとまったデータを作る方法
+// const coffee = {　　        //{};がオブジェクトを作るための記号
+//   name: "Chocolate Mocha", //kyey-value形式でデータを作っていく
+//   price: 100,           　　//またの名前でプロパティとも言う
+//   size: "M",          　　　//key-valueの間には「:」を入れる
+//   isHot: true,       　　　 //サイズプロパティは、オブジェクトの中にあるデータのサイズ
+//   toppings: ["chocolate", "whipped cream"],　//配列の中にデータを入れることもできる
+//   nutrition: {                       // オブジェクトの中にオブジェクトを入れることもできる
+//     calories: 450,
+//     sugars: 50,
 //   },
 // };
 // console.log(coffee.name); //オブジェクトのプロパティにアクセス
@@ -63,31 +120,31 @@
 // coffee.barista = "Susumu"; //オブジェクトのプロパティを追加
 // console.log(coffee.barista); //オブジェクトのプロパティにアクセス
 
-// //nullとundefinedの違い
+//１３．nullとundefinedの違いについて
 // let useInfo = null; //nullは値がないことを示す
-// console.log(userInfo); //nullは値がないことを示す
-// userInfo = undefined; //undefinedは値がerrorであることを示す
+// console.log(userInfo); //nullは値がないことを示す//undefinedは値がないことを示す。２種類ある
+// userInfo = undefined; //undefinedは値がerrorであることを示す。基本は何もない時はnullを使う
 // console.log(userInfo); //undefinedは値がerrorであることを示す
 // //undefinedは変数が宣言されているが値が設定されていないことを示す
 
-
+//typeof演算子を使って、変数の型を調べる方法
 
 //関数の宣言
-const value = "hello"
-function add(a, b) { //関数の名前はadd、引数はaとb
-    const value = a + b; //aとbを足した値をvalueに代入
-    //関数の引数を受け取る
-    //console.log(value); //関数の引数を表示
-    //hello();
-    return value; //戻り値を返す 
-    //return文の後に書いても無視されてしまう。
-    console.log("hello"); //return文の後に書いても無視されてしまう。    
-}
-//関数の呼び出し
+// const value = "hello"
+// function add(a, b) { //関数の名前はadd、引数はaとb
+//     const value = a + b; //aとbを足した値をvalueに代入
+//     //関数の引数を受け取る
+//     //console.log(value); //関数の引数を表示
+//     //hello();
+//     return value; //戻り値を返す 
+//     //return文の後に書いても無視されてしまう。
+//     console.log("hello"); //return文の後に書いても無視されてしまう。    
+// }
+// //関数の呼び出し
 
-const returnedVaoue = add(5, 5); //関数の戻り値を受け取る
-console.log(returnedVaoue); //関数の戻り値を表示
-console.log(value); //関数の戻り値を表示
+// const returnedVaoue = add(5, 5); //関数の戻り値を受け取る
+// console.log(returnedVaoue); //関数の戻り値を表示
+// console.log(value); //関数の戻り値を表示
 
 
 //CTRL + s で保存
